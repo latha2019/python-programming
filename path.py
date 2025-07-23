0d2cb39e4e2a1ecc948387445ca8bc0d8f4dabf2
latha2019 <lathabgowda2@gmail.com>
2025-07-23 21:41:47 +0530

    Update path.py

--- path.py
+++ path.py
@@ -1,19 +1,56 @@
-import os
+# import os
+#
+# def dirname(path):
+#     return os.path.dirname(path)
+#
+# def basename(path):
+#     return os.path.basename(path)
+#
+# def extname(path):
+#     _,ext = os.path.splitext(path)
+#     return ext
+
+def Filename(path):
+    i = len(path)-1
+    filename=""
+    while i > 0:
+        if path[i] == '/' or path[i] == '//':
+            break
+
+        filename = path[i] + filename
+        i -= 1
+    return filename
 
 def dirname(path):
-    return os.path.dirname(path)
+    i = len(path) - 1
+    directory = ""
+    while i > 0:
+        if path[i] == '/' or path[i] == '//':
+            break
+        # filename = path[i] + filename
+        i -= 1
 
-def basename(path):
-    return os.path.basename(path)
+    for j in range(i):
+        directory += path[j]
+
+    return directory
 
 def extname(path):
-    _,ext = os.path.splitext(path)
-    return ext
+    k = len(path) - 1
+    filename = ""
+    while k > 0:
+        if path[k] == '/' or path[k] == '//':
+            break
+        filename = path[k] + filename
+        k -= 1
 
-# location = "/home/apple/Downloads/story.txt"
-# dirloc = dirname(location)
-# print(dirloc)
-# filename = basename(location)
-# print(filename)
-# filext = extname(location)
-# print(filext)
+    ext = filename.split(".")
+    return ext[-1]
+
+location = "/home/apple/Downloads/story.csv"
+dirloc = dirname(location)
+print(f"Directory: {dirloc}")
+filename = Filename(location)
+print(f"filename: {filename}")
+filext = extname(location)
+print(f"file ext: {filext}")
