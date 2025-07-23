3dbce53b360549c322e4dcacc439920df8923ec2
latha2019 <lathabgowda2@gmail.com>
2025-07-23 13:50:57 +0530

    Update Monthof30.py

--- Monthof30.py
+++ Monthof30.py
@@ -1,16 +1,17 @@
 
-def freedays(a, b):
+def freedays(booked, travel):
     """ function to takes two lists as parameters and
-            returns a list of days those are free to study"""
-    occupied = a + b
-    print("occupied dates for the month:",occupied)
+    returns a list of days those are free to study
+    """
+    occupied = booked + travel
+    print("occupied dates for the month:", occupied)
     study = []
-    for ftstu in range(1,31):
-        if ftstu not in occupied:
-            study.append(ftstu)
+    for days in range(1, 31):
+        if days not in occupied:
+            study.append(days)
     return study
 
 booked = [ 1, 3, 9, 12, 13, 18, 26, 27, 28, 29 ]
 travel = [ 4, 5, 15, 16, 21, 22 ]
 res = freedays(booked, travel)
-print(" list of days free to study in the month: ",res)
+print(" list of days free to study in the month: ", res)
