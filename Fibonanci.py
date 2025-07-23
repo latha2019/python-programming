d40e7ee57224309ea8feca5c44382fb15569bb5e
latha2019 <lathabgowda2@gmail.com>
2025-07-23 13:35:51 +0530

    Update Fibonanci.py

--- Fibonanci.py
+++ Fibonanci.py
@@ -1,16 +1,13 @@
 # Python program to print the first 12 "even" Fibonacci numbers
 # Assume the first two Fibonacci numbers to be 1 and 1
-num1 = 1
-num2 = 1
+first = 1
+sec = 1
 count = 1
 print("The first 12 'even' Fibonacci numbers are:")
-for i in range(0,50):
-    num3 = num1 + num2
-    # print(num3)
-    if num3 % 2 == 0:
-        print(num3,"\t")
+while count <= 12:
+    third = first + sec
+    if third % 2 == 0:
+        print(third)
         count = count + 1
-    if count > 12:
-        break
-    num1 = num2
-    num2 = num3
+
+    first, sec = sec, third
